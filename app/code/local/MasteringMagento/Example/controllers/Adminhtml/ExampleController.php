d22a94348927b2919baaed9674b7254a91006486
Subash Basnet <subashbasnet@Subashs-iMac.local>
2017-03-02 16:22:11 +0200

    Example menu added in the admin panel

---
+++ app/code/local/MasteringMagento/Example/controllers/Adminhtml/ExampleController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * app/code/local/MasteringMagento/Example/controllers/ExampleController.php
+ *
+ * This is description
+ *
+ * NOTICE OF LICENSE
+ *
+ * @author      Subash Basnet
+ * @category    MasteringMagento
+ * @package     Example
+ * @copyright   Copyright (c) 2017 
+ */
+class MasteringMagento_Example_Adminhtml_ExampleController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+
+        return $this->renderLayout();
+    }
+}
