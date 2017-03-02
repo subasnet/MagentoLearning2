c123fe0a50ea2e85b537d305731acf37dffd8cc2
Subash Basnet <subashbasnet@Subashs-iMac.local>
2017-03-02 15:33:01 +0200

    Example module created

---
+++ app/code/local/MasteringMagento/Example/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Magento
+ * app/code/local/MasteringMagento/Example/Helper/Data.php
+ *
+ * Description of the file
+ *
+ * @author      Subash Basnet
+ * @category    MasteringMagento
+ * @package     Example
+ * @copyright   Copyright (c) 2017 
+ */
+class MasteringMagento_Example_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    // Empty helper class
+}
